ee02c194a9fe02484065aa7a4f0351b97cc74da9
tomschofield <tomschofieldart@gmail.com>
2013-03-29 10:52:05 +0000

    added parts list and update css

---
+++ NAI_SERVER/myform.php
@@ -0,0 +1,28 @@
+<?php
+if($_POST['formSubmit'] == "Submit")
+{
+	$errorMessage = "";
+	
+	if(empty($_POST['formMovie']))
+	{
+		$errorMessage .= "<li>You forgot to enter a movie!</li>";
+	}
+	if(empty($_POST['formName']))
+	{
+		$errorMessage .= "<li>You forgot to enter a name!</li>";
+	}
+	
+	$varMovie = $_POST['formMovie'];
+	$varName = $_POST['formName'];
+
+	if(empty($errorMessage)) 
+	{
+		$fs = fopen("mydata.csv","a");
+		fwrite($fs,$varName . ", " . $varMovie . "\n");
+		fclose($fs);
+		
+		header("Location: thank-you.html");
+		exit;
+	}
+}
+?>
